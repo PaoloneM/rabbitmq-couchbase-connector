3fff7ddd76a68480ee7342fa0f0677165034c7c3
Paolo Morgano <paolo.morgano@gmail.com>
2020-04-16 14:35:35 +0200

    fix CB_URL env parsing

--- src/main/java/com/ff3d/rabbitmq_couchbase_connector/CbRabbitConnector.java
+++ src/main/java/com/ff3d/rabbitmq_couchbase_connector/CbRabbitConnector.java
@@ -4,55 +4,62 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
 import com.couchbase.client.core.env.NetworkResolution;
 import com.couchbase.client.dcp.config.CompressionMode;
 
 class CbRabbitConnector {
-    public static void main(String[] args) throws IOException, TimeoutException {
+    public static void main(final String[] args) throws IOException, TimeoutException {
 
         System.out.println("Welcome to FF3D.COM Couchbase Rabbit Connector");
 
-        // Test name resolution if cluster headless server
-        InetAddress address = InetAddress.getByName(Constants.COUCHBASE_CLUSTER_SERVICE); 
-        System.out.println(address.getHostAddress()); 
+        System.out.println("******************************Environment Vars*****************************");
+        Map<String, String> env = System.getenv();
+        for (String envName : env.keySet()) {
+            System.out.format("%s=%s%n", envName, env.get(envName));
+        }
 
-        String list = System.getenv(Constants.COUCHBASE_CLUSTER);
-        List<String> couchbaseCluster = Arrays.asList(list.split(","));
-        String bucket = System.getenv(Constants.COUCHBASE_BUCKET);
-        String bucketUser = System.getenv(Constants.COUCHBASE_BUCKET_USER);
-        String bucketPassword = System.getenv(Constants.COUCHBASE_BUCKET_PASSWORD);
-        long cbConnectionTimeout = Long.parseLong(System.getenv(Constants.COUCHBASE_CONN_TIMEOUT));
-        long cbPersistencePollIntv = Long.parseLong(System.getenv(Constants.COUCHBASE_PERSISTENCE_POLL_INTV));
-        int flowCtrlBuffBytes = Integer.parseInt(System.getenv(Constants.COUCHBASE_FLOWCTRL_BUFF_BYTES));
-        String rabbitHost = System.getenv(Constants.RABBIT_HOST);
-        int rabbitPort = Integer.parseInt(System.getenv(Constants.RABBIT_PORT));
-        String rabbitUser = System.getenv(Constants.RABBIT_USER);
-        String rabbitPassword = System.getenv(Constants.RABBIT_PASSWORD);
-        String stateFilePath = System.getenv(Constants.STATE_FILE_PATH);
+        // Test name resolution if cluster headless server
+        final InetAddress address = InetAddress.getByName(System.getenv(Constants.COUCHBASE_CLUSTER_SERVICE));
+        System.out.println(address.getHostAddress());
+
+        final String list = System.getenv(Constants.COUCHBASE_CLUSTER);
+        final List<String> couchbaseCluster = Arrays.asList(list.split(","));
+        final String bucket = System.getenv(Constants.COUCHBASE_BUCKET);
+        final String bucketUser = System.getenv(Constants.COUCHBASE_BUCKET_USER);
+        final String bucketPassword = System.getenv(Constants.COUCHBASE_BUCKET_PASSWORD);
+        final long cbConnectionTimeout = Long.parseLong(System.getenv(Constants.COUCHBASE_CONN_TIMEOUT));
+        final long cbPersistencePollIntv = Long.parseLong(System.getenv(Constants.COUCHBASE_PERSISTENCE_POLL_INTV));
+        final int flowCtrlBuffBytes = Integer.parseInt(System.getenv(Constants.COUCHBASE_FLOWCTRL_BUFF_BYTES));
+        final String rabbitHost = System.getenv(Constants.RABBIT_HOST);
+        final int rabbitPort = Integer.parseInt(System.getenv(Constants.RABBIT_PORT));
+        final String rabbitUser = System.getenv(Constants.RABBIT_USER);
+        final String rabbitPassword = System.getenv(Constants.RABBIT_PASSWORD);
+        final String stateFilePath = System.getenv(Constants.STATE_FILE_PATH);
 
         final DCPStream stream = new DCPStream();
-        
+
         System.out.println("Configuring stream");
 
         stream.init(couchbaseCluster, bucket, bucketUser, bucketPassword, cbConnectionTimeout, NetworkResolution.AUTO,
                 CompressionMode.ENABLED, cbPersistencePollIntv, flowCtrlBuffBytes, rabbitHost, rabbitPort, rabbitUser,
                 rabbitPassword, stateFilePath);
-        
+
         System.out.println("Starting stream");
-        try {       
+        try {
             stream.start();
             Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
                 public void run() {
                     try {
                         stream.stop();
-                    } catch (IOException e) {
+                    } catch (final IOException e) {
                         e.printStackTrace();
                     }
                 }
             }));
-        } catch (Exception e) {
+        } catch (final Exception e) {
             e.printStackTrace();
             System.exit(1);
         }
